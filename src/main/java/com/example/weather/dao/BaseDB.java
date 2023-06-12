3c850c1adefbe5b13bbf37a77b4ef31f09a28325
Pgfishs <135108083+Pgfishs@users.noreply.github.com>
2023-06-12 20:55:41 +0800

    6.12

---
+++ src/main/java/com/example/weather/dao/BaseDB.java
@@ -0,0 +1,20 @@
+package com.example.weather.dao;
+
+import com.example.weather.eneity.UserInfoEntity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDB {
+    Connection conn;
+    BaseDB() throws ClassNotFoundException, SQLException {
+        String user="root";
+        String password="pgy040427";
+        String URL ="jdbc:mysql://localhost:3306/weathersearchsystem?" +
+                "useUnicode=true&characterEncoding=utf8&&useSSL=false";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn = DriverManager.getConnection(URL,user,password);
+    }
+
+}
--- src/main/java/com/example/weather/dao/UserDB.java
+++ src/main/java/com/example/weather/dao/UserDB.java
@@ -1,5 +1,12 @@
 package com.example.weather.dao;
 
-public class UserDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class UserDB extends BaseDB {
+    UserDB() throws ClassNotFoundException, SQLException {
+    }
+
     //TODO UserDBInfo modify
 }
